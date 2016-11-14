5fefda3aa08de10f37f0dea43431afbd85e1aff6
jimeni72 <info@tpesoft.com>
2016-11-14 11:48:56 +0330

    First commit about version 3

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets, vendor/assets,
+// or any plugin's vendor/assets directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require camera
+//= require device.min
+//= require html5shiv
+//= require index
+//= require isotope.min
+//= require jquery.cookie
+//= require jquery.easing.1.3
+//= require jquery.equalheights
+//= require jquery
+//= require jquery.mobile.customized.min
+//= require jquery.mobilemenu
+//= require jquery.mousewheel.min
+//= require jquery.simplr.smoothscroll.min
+//= require jquery.ui.totop
+//= require jquery.unveil
+//= require jquery-1.11.0.min
+//= require jquery-migrate-1.2.1
+//= require modal
+//= require owl.carousel
+//= require script
+//= require superfish
+//= require TMForm
+//= require tmstickup
+//= require_tree .
